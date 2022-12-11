d5cb8c3ddd25114df416416ceed9966b42942317
ab185588ncr <119924080+ab185588ncr@users.noreply.github.com>
2022-12-12 00:24:36 +0530

    Add files via upload

--- 2.sql
+++ 2.sql
@@ -1,11 +1,11 @@
-create type employee_type as table(empid int,empname varchar(255),empsalary int);
-drop table employee;
-create table employee(id int,nam varchar(255),salary int);
-insert into employee values(1,'abhi',100000),(3,'nithin',300000)
+create type employees_type as table(empid int,empname varchar(255),empsalary int);
+--drop table employees;
+create table employees(id int,nam varchar(255),salary int);
+insert into employees values(1,'abhi',100000),(3,'nithin',300000)
 
-select * from employee;
+select * from employees;
 
-create or alter procedure emp_proc (@emp employee_type readonly)
+create or alter procedure emp_proc @emp employees_type readonly
 as
 begin
 	declare 
@@ -17,30 +17,29 @@ begin
 	select @temp_name = empname from @emp;
 	select @temp_sal = empsalary from @emp;
 	
-	select @cnt = count(*) from employee where id=@temp_id;
+	set  @cnt = (select count(*) from employees where id=@temp_id);
 
 	if @cnt > 0
-	update employee set nam = @temp_name ,salary = @temp_sal  where id = @temp_id;
+	update employees set nam = @temp_name ,salary = @temp_sal  where id = @temp_id;
 	else 
-	insert into employee values(@temp_id,@temp_name,@temp_sal);
+	insert into employees select * from @emp;
 end
 
 
 
 -- insert new values 
-declare @temp_type_table employee_type
+declare @temp_type_table employees_type
 insert into @temp_type_table values(2,'meghanath',250000);
-insert into @temp_type_table values(3,'nithin',430000);
 exec emp_proc @temp_type_table
 
 
-select * from employee;
+select * from employees;
 
 -- updating old values 
-declare @temp_type_table_1 employee_type
-insert into @temp_type_table values(2,'meghanath',500000);
-insert into @temp_type_table values(3,'nithin',830000);
+declare @temp_type_table_1 employees_type
+insert into @temp_type_table_1 values(2,'meghanath',500000);
 exec emp_proc @temp_type_table_1
 
-select * from employee;
+select * from employees;
 
+f
