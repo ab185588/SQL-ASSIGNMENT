1adf4af1ed446a6e5108dd7b14d8c22ecd8999f7
ab185588ncr <119924080+ab185588ncr@users.noreply.github.com>
2022-12-14 00:32:03 +0530

    Add files via upload

--- 5.sql
+++ 5.sql
@@ -1,18 +1,3 @@
-/*
-Redarging this question i have created a table where it has columns as eid,ename,mid where eid is employee id and mid 
-is the manager id.
-
-
-I have written this sql query to show heirarchy from the employee id 
-which  we have to set it in the code 
-
-for example if we give employee id as 6 
-
-it shows simran reports to mahesh, mahesh reports to bro , and the bro is superior authority
-
-*/
-
-
 use a1;
 drop table employee;
 create table employee(
@@ -42,7 +27,7 @@ Update employee Set mid = 4 Where ename IN ('don')
 select * from employee
 
 declare @id int;
-set @id = 6 ;
+set @id = 5 ;
 
 with emp_cte as 
 (
@@ -54,5 +39,6 @@ with emp_cte as
    select e.eid,e.ename,e.mid
    from employee e join emp_cte ec on e.eid = ec.mid 
 )
+
 select e1.ename as EmployeeNAme,coalesce(e2.ename,'Superior Authority' )as ManagerNAme 
-from emp_cte e1 left join emp_cte e2 on e1.mid = e2.eid;
+from emp_cte as e1 left join emp_cte as e2 on e1.mid = e2.eid;
